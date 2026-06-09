075844741dce135330cec834d2411b461f47c249
zz-pc <814647964@qq.com>
2026-06-09 17:58:36 +0800

    add X11 authority

--- src/dddmr_beginner_guide/run_x64_gazebo.bash
+++ src/dddmr_beginner_guide/run_x64_gazebo.bash
@@ -2,12 +2,25 @@
 
 xhost +local:docker
 
+# docker run -it \
+#     --privileged \
+#     --network=host \
+#     --env="DISPLAY" \
+#     --env="QT_X11_NO_MITSHM=1" \
+#     --volume="/tmp:/tmp" \
+#     --volume="/dev:/dev" \
+#     --name="dddmr_x64_gazebo" \
+#     dddmr_gz:x64
+
+# server X11 setting
 docker run -it \
-    --privileged \
-    --network=host \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp:/tmp" \
-    --volume="/dev:/dev" \
-    --name="dddmr_x64_gazebo" \
-    dddmr_gz:x64
+  --privileged \
+  --network=host \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -e XAUTHORITY=/root/.Xauthority \
+  -v $HOME/.Xauthority:/root/.Xauthority:ro \
+  -v /tmp:/tmp \
+  -v /dev:/dev \
+  --name dddmr_x64_gazebo \
+  dddmr_gz:x64
--- src/dddmr_beginner_guide/run_x64_navigation.bash
+++ src/dddmr_beginner_guide/run_x64_navigation.bash
@@ -2,14 +2,29 @@
 
 xhost +local:docker
 
+# docker run -it \
+#     --privileged \
+#     --network=host \
+#     --env="DISPLAY" \
+#     --env="QT_X11_NO_MITSHM=1" \
+#     --volume="/tmp:/tmp" \
+#     --volume="/dev:/dev" \
+#     --volume="${HOME}/dddmr_navigation:/root/dddmr_navigation" \
+#     --volume="${HOME}/dddmr_bags:/root/dddmr_bags" \
+#     --name="dddmr_x64_navigation" \
+#     dddmr:x64
+
+# server X11 setting
 docker run -it \
     --privileged \
     --network=host \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp:/tmp" \
-    --volume="/dev:/dev" \
-    --volume="${HOME}/dddmr_navigation:/root/dddmr_navigation" \
-    --volume="${HOME}/dddmr_bags:/root/dddmr_bags" \
-    --name="dddmr_x64_navigation" \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -e XAUTHORITY=/root/.Xauthority \
+    -v $HOME/.Xauthority:/root/.Xauthority:ro \
+    -v /tmp:/tmp \
+    -v /dev:/dev \
+    -v "${HOME}/dddmr_navigation:/root/dddmr_navigation" \
+    -v "${HOME}/dddmr_bags:/root/dddmr_bags" \
+    --name dddmr_x64_navigation \
     dddmr:x64
